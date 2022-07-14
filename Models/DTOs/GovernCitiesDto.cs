25901d44eaf57c55749d9bbc40afcac188b519d8
Ahmed Hassan <58051308+KharKhasino@users.noreply.github.com>
2022-07-14 08:57:13 +0200

    Add files via upload

---
+++ Models/DTOs/GovernCitiesDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shipping.API.Models.DTOs
+{
+    public class GovernCitiesDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public Dictionary<string, int> Cities { get; set; } = new Dictionary<string, int>();
+    }
+}
---
+++ Models/DTOs/OrderDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Shipping.API.Models.DTOs
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+
+        public int CustomerPhoneNo { get; set; }
+        public string ShippingAdress { get; set; }
+        public string Status { get; set; }
+        public DateTime TimeCreated { get; set; }
+        public string Governorate { get; set; }
+        public string City { get; set; }
+        public string ShippingType { get; set; }
+        public string PaymentMethod { get; set; }
+
+    }
+}
